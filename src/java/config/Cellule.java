705f2a3df30e73e20ac41b13a6b3a44572ed6c14
Mina <xo140597@gmail.com>
2023-11-17 12:29:10 +0100

    ajout de base

--- src/java/config/Cellule.java
+++ src/java/config/Cellule.java
@@ -12,11 +12,14 @@ public class Cellule {
     // 1 pour dire que c'est le chemin des monstres
     // 2 pour dire que c'est l'eau
     // 3 pour dire que c'est un arbre
+    // 4 poyr dire que c'est la base
+
     public static Cellule Cell (int i ){
         if ( i == 0 ) return new Cellule(false , 0 ) ;
         if ( i == 1 ) return new Cellule(true, 1 ) ;
         if ( i == 2 ) return new Cellule(false, 2 ) ;
         if ( i == 3 ) return new Cellule(false, 3 ) ;
+        if (  i== 4 ) return new Cellule(true , 4) ;
         return null ;
     }
     public boolean isRoad() {
