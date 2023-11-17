70a5362e1604237abc5f28322d8cbf952b458a92
Mina <xo140597@gmail.com>
2023-11-17 11:31:59 +0100

    amelioration de MonsN.java et implementation des types de montres dans la classe

--- src/java/model/MonsN.java
+++ src/java/model/MonsN.java
@@ -4,15 +4,15 @@ import java.util.Random;
 
 public class MonsN extends Monster
 {
-    private static double[][] mulp =
-                    {
-                            {1, 1},
-                            {1.5, 1.1},
-                            {1.8, 1.3},
-                            {2, 1.5}
-                    }; // live, speed. Exemples a chane
 
-    private static int[][] res_persentage =
+    private static double[][][] mulp =
+            {
+                    {{1, 1}, {1.5, 1.1}, {1.8, 1.3}, {2, 1.5}},
+                    {{1, 0.9}, {1.5, 0.9}, {1.8, 0.85}, {2, 0.8}},
+                    {{1, 1.6}, {1.5, 1.45}, {1.8, 1.3}, {2, 1.1}}
+            }; // live, speed. Exemples a chane
+
+    private static int[][] res_persentage_avoir =
             {
                     {100, 0, 0, 0},
                     {70, 10, 10, 10},
@@ -20,14 +20,18 @@ public class MonsN extends Monster
                     {10, 30, 30, 30}
             }; // NONE, BULLET, ARROW, FIRE. Pourcentage pour avoir une resistance.
 
+    private static double[] resistance_pourcentage =
+            { 0 , 0.1 ,0.3 ,0.5 } ;
+    private int type ; // 0 est lent ; 1 est normal ; 2 est rapide
     private static String[] res_list = {"NONE", "BULLET", "ARROW", "FIRE"};
 
     private String resistance = "";
 
-    public MonsN(String name , double live , double speed , int money , double x, double y, int niveau)
+    public MonsN(String name , double live , double speed , int money , double x, double y, int niveau,int type)
     {
-        super(name, live * mulp[niveau][0], speed * mulp[niveau][1], money, niveau, x, y);
+        super(name, live * mulp[type][niveau][0], speed * mulp[type][niveau][1], money, niveau, x, y);
         choixResistance();
+        this.type = type ;
     }
 
     private void choixResistance()
@@ -38,7 +42,7 @@ public class MonsN extends Monster
         for (int i = 0; i < res_list.length-1; i++) { // Parcourir les pourcentages des resistances
             int res_pour = 0; // Pourcentage de resistance
             for (int j = 0; j <= i; j++) {
-                res_pour += res_persentage[niveau][j];
+                res_pour += res_persentage_avoir[niveau][j];
             }
 
             if (choix_resistance < res_pour) {
