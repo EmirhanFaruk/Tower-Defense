9aae5ff09b2993fa579a3f5c0ae1293eff112c7b
Mina <xo140597@gmail.com>
2023-12-26 16:54:16 +0100

    petit fix pour les constructeurs des differentes tours

--- src/java/model/tour/Archer.java
+++ src/java/model/tour/Archer.java
@@ -1,7 +1,7 @@
 package model.tour;
 
 public class Archer extends Tour {
-    public Archer(String name, int prix, int degats, int level, int x, int y, int range, int time) {
-        super(name, prix, degats, level, x, y, range, time);
+    public Archer( int prix, int degats, int level, int x, int y, int range, int time) {
+        super("Archer", prix, degats, level, x, y, range, time);
     }
 }
