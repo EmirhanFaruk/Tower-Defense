14492c24bb431a3791cee5b2555c94324aeffa70
EmirhanFaruk <emirhansagir74@gmail.com>
2023-12-27 23:30:11 +0100

    Done the system, will try.

--- src/java/model/monster/MonsterSpawner.java
+++ src/java/model/monster/MonsterSpawner.java
@@ -8,15 +8,18 @@ import java.util.Random;
 
 public class MonsterSpawner
 {
-    private double in_wave_timer;
-    private final double in_wave_timer_max;
+    private double in_serie_timer;
+    private final double in_serie_timer_max;
     private double between_wave_timer;
     private final double between_wave_timer_max;
 
     private boolean in_wave = false;
+    private int serie_count_max = 1;
+    private int serie_count = 0;
     private int wave_count;
-    private int wave_count_max;
-    private int monster_per_wave;
+    private final int wave_count_max; // pour refaire toutes les vagues
+    private final int[] monster_per_serie_max = {5, 15};
+    private int monster_per_serie;
     private double monster_timer;
     private final double monster_timer_max;
 
@@ -26,18 +29,27 @@ public class MonsterSpawner
 
     private Coordinate start;
 
-    public MonsterSpawner(double in_wave_timer_max, double between_wave_timer_max, int wave_count_max, int monster_per_wave, double monster_timer_max, MapConfig map_config, Coordinate start)
+    /**
+     * Constructeur de MonsterSpawner. Initialisation des attributs.
+     * @param in_serie_timer_max le temps max pour faire apparaitre et tuer les monstres
+     * @param between_wave_timer_max le temps max pour attendre le prochain vague
+     * @param wave_count_max maximum nombre de vagues
+     * @param monster_timer_max le temps max entre l'apparition des monstres dans une serie
+     * @param map_config map_config pour produire les monstre
+     * @param start la coordonnée de debut
+     */
+    public MonsterSpawner(double in_serie_timer_max, double between_wave_timer_max, int wave_count_max, double monster_timer_max, MapConfig map_config, Coordinate start)
     {
-        this.in_wave_timer_max = in_wave_timer_max;
-        in_wave_timer = in_wave_timer_max;
+        this.in_serie_timer_max = in_serie_timer_max;
+        in_serie_timer = in_serie_timer_max;
 
         this.between_wave_timer_max = between_wave_timer_max;
         between_wave_timer = between_wave_timer_max;
 
         this.wave_count_max = wave_count_max;
-        this.wave_count = wave_count_max;
+        this.wave_count = 0;
 
-        this.monster_per_wave = monster_per_wave;
+        this.monster_per_serie = monster_per_serie_max[serie_count % 2];
         this.monster_timer_max = monster_timer_max;
 
         monster_timer = monster_timer_max;
@@ -48,7 +60,7 @@ public class MonsterSpawner
     }
 
     /**
-     * Mettre a jour
+     * Mettre a jour tous.
      * @param delta_time delta time
      * @param monsters liste de monstre a ajouter
      */
@@ -62,14 +74,45 @@ public class MonsterSpawner
     }
 
     /**
-     * Gerer les timers
+     * Gerer les timers.
+     * | between_wave_timer | in_serie_timer | repeat
      * @param delta_time delta time
      */
     private void timerHandler(long delta_time)
     {
         double delta_double = ((double)delta_time)/1000000000;
-        in_wave_timer -= delta_double;
-        
+        if(between_wave_timer > 0)
+        {
+            between_wave_timer -= delta_double;
+            if(between_wave_timer <= 0)
+            {
+                in_serie_timer = in_serie_timer_max;
+            }
+        }
+        else
+        {
+            if(in_serie_timer > 0)
+            {
+                in_serie_timer -= delta_double;
+                if (monster_per_serie > 0 && monster_timer > 0)
+                {
+                    monster_timer -= delta_double;
+                }
+            }
+            else
+            {
+                serie_count++;
+                if(serie_count == serie_count_max)
+                {
+                    between_wave_timer = between_wave_timer_max;
+                    serie_count = 0;
+                }
+                else
+                {
+                    in_serie_timer = in_serie_timer_max;
+                }
+            }
+        }
     }
 
     /**
@@ -78,7 +121,25 @@ public class MonsterSpawner
      */
     private void spawnWave(ArrayList<Monster> monsters)
     {
-
+        if(in_wave)
+        {
+            if(in_serie_timer > 0 && monster_per_serie > 0)
+            {
+                // If in a serie, has monsters to spawn and monster timer is up, put a monster and renew timer.
+                if(monster_timer <= 0)
+                {
+                    spawnMonster(monsters);
+                    monster_per_serie--;
+                    monster_timer = monster_timer_max;
+                }
+            }
+            if(monster_per_serie == 0 && monsters.isEmpty())
+            {
+                // If no more monsters, pass to the other serie in 3 seconds.
+                monster_per_serie = -1;
+                in_serie_timer = 3;
+            }
+        }
     }
 
     /**
