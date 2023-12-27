e2cdb795399cbe2138b10bf5eeeda26d6500bf32
EmirhanFaruk <emirhansagir74@gmail.com>
2023-12-27 19:16:31 +0100

    Start of timer handler

--- src/java/model/monster/MonsterSpawner.java
+++ src/java/model/monster/MonsterSpawner.java
@@ -24,7 +24,9 @@ public class MonsterSpawner
 
     private MapConfig map_config;
 
-    public MonsterSpawner(double in_wave_timer_max, double between_wave_timer_max, int wave_count_max, int monster_per_wave, double monster_timer_max, MapConfig map_config)
+    private Coordinate start;
+
+    public MonsterSpawner(double in_wave_timer_max, double between_wave_timer_max, int wave_count_max, int monster_per_wave, double monster_timer_max, MapConfig map_config, Coordinate start)
     {
         this.in_wave_timer_max = in_wave_timer_max;
         in_wave_timer = in_wave_timer_max;
@@ -41,25 +43,53 @@ public class MonsterSpawner
         monster_timer = monster_timer_max;
 
         this.map_config = map_config;
+
+        this.start = start;
     }
 
+    /**
+     * Mettre a jour
+     * @param delta_time delta time
+     * @param monsters liste de monstre a ajouter
+     */
+    public void update(long delta_time, ArrayList<Monster> monsters)
+    {
+        timerHandler(delta_time);
+        if(in_wave)
+        {
+            spawnWave(monsters);
+        }
+    }
 
+    /**
+     * Gerer les timers
+     * @param delta_time delta time
+     */
+    private void timerHandler(long delta_time)
+    {
+        double delta_double = ((double)delta_time)/1000000000;
+        in_wave_timer -= delta_double;
+        
+    }
 
-    public void spawnWave(ArrayList<Monster> monsters, int level, Coordinate coordinate)
+    /**
+     * Produire une vague des monstres
+     * @param monsters liste de monstre a ajouter
+     */
+    private void spawnWave(ArrayList<Monster> monsters)
     {
 
     }
 
     /**
-     * Makes random type monsters. For now.
-     * @param monsters monster list to add to
-     * @param coordinate start coordinate
+     * Produire des monstres de type au hazard.
+     * @param monsters liste de monstre a ajouter
      */
-    public void spawnMonster(ArrayList<Monster> monsters, Coordinate coordinate)
+    private void spawnMonster(ArrayList<Monster> monsters)
     {
         Random random = new Random();
         int type = random.nextInt(3);
         int money = random.nextInt(20);
-        monsters.add(new Monster("Monster of wave " + wave_count, 20 * niveau, 5, money, niveau, coordinate.copy(), type, map_config));
+        monsters.add(new Monster("Monster of wave " + wave_count, 20 * niveau, 5, money, niveau, start.copy(), type, map_config));
     }
 }
