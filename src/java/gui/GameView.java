adf6ea0c27e1c3d64ae6fe067d1b4ba850ba11d6
EmirhanFaruk <emirhansagir74@gmail.com>
2023-12-28 16:51:35 +0100

    Mettre les descriptions des funcs

--- src/java/gui/GameView.java
+++ src/java/gui/GameView.java
@@ -12,11 +12,17 @@ public class GameView extends JPanel
     private MapConfig map_config;
     private MonsterSpawner monster_spawner;
 
+    /**
+     * Constructeur de GameView, assigner les attributs
+     */
     public GameView()
     {
         // Produire les attributs etc...
     }
 
+    /**
+     * Une func qui fait rouler le mainLoop
+     */
     public void run()
     {
         while(running)
@@ -24,17 +30,27 @@ public class GameView extends JPanel
             mainLoop();
         }
     }
+
+    /**
+     * Une func qui contient les mise a jours de code et de graphics de jeu
+     */
     private void mainLoop()
     {
         updateCode();
         updateGraphs();
     }
 
+    /**
+     * Une function qui ensemble les mise a jours de monstres, les tours...
+     */
     private void updateCode()
     {
 
     }
 
+    /**
+     * Une func qui met a jour le panel pour afficher les elements de jeu
+     */
     private void updateGraphs()
     {
 
