c8655f46e9310532b348320b3a9c725d303835f7
EmirhanFaruk <emirhansagir74@gmail.com>
2023-12-28 16:46:01 +0100

    Debut de modele de gameview

--- src/java/gui/GameView.java
+++ src/java/gui/GameView.java
@@ -1,4 +1,43 @@
 package gui;
-public class GameView  {
+
+import config.MapConfig;
+import model.monster.MonsterSpawner;
+
+import javax.swing.*;
+
+public class GameView extends JPanel
+{
+    private boolean running;
+
+    private MapConfig map_config;
+    private MonsterSpawner monster_spawner;
+
+    public GameView()
+    {
+        // Produire les attributs etc...
+    }
+
+    public void run()
+    {
+        while(running)
+        {
+            mainLoop();
+        }
+    }
+    private void mainLoop()
+    {
+        updateCode();
+        updateGraphs();
+    }
+
+    private void updateCode()
+    {
+
+    }
+
+    private void updateGraphs()
+    {
+
+    }
 
 }
