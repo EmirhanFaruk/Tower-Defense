d15f074d0509dc9c12f7c711c32f47492aadd8de
Mina <xo140597@gmail.com>
2024-01-03 22:53:53 +0100

    changement de nom des classes Arme.java et Artillerie.java en Soldat.java et Catapulte.java

--- src/java/model/tour/Catapulte.java
+++ src/java/model/tour/Catapulte.java
@@ -4,15 +4,14 @@ import model.monster.Monster;
 
 import java.util.ArrayList;
 
-public class Arme extends Tour {
-    public Arme( int level, int x, int y ) {
-        super( "arme" , 10 , 10 , level, x , y , 2, 6 , 100 );
+public class Catapulte extends Tour {
+    public Catapulte(int level, int x, int y) {
+        super("catapulte", 80, 90 , level , x , y , 5 , 7 , 10000 );
     }
 
-    public Arme( int level ) {
-        super( "arme" , 10 , 10 , level ,100 , 6 , 100 );
+    public Catapulte(int level ) {
+        super("catapulte", 80, 90 , level , 600 , 7 , 10000 );
     }
-
     @Override
     public void attaquer(ArrayList<Monster> monsters) {
         super.attaquer(monsters);
