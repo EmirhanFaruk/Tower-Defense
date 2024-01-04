2f410c15844bd109831cf2c7e024841f123cac6e
EmirhanFaruk <emirhansagir74@gmail.com>
2024-01-04 16:28:38 +0100

    avancements dans affichage

--- src/java/gui/game/GameScreen.java
+++ src/java/gui/game/GameScreen.java
@@ -8,6 +8,7 @@ import gui.game.paint.MonsterPaintable;
 import gui.game.paint.TourPaintable;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class GameScreen extends JPanel
@@ -31,12 +32,79 @@ public class GameScreen extends JPanel
         makeCaros(map_config);
     }
 
-    private void makeMap_panel()
+    private JPanel makeMap_panel()
     {
         JPanel res = new JPanel();
-
+        return res;
     }
 
+
+    private JLabel makeMessagePanel()
+    {
+        JLabel message_panel = new JLabel("Welcome to the game!");
+        // Brown background
+        message_panel.setBackground(new Color(102, 61, 20));
+        message_panel.setForeground(Color.ORANGE);
+
+        return message_panel;
+    }
+
+    /**
+     * "Placehoder" pour le menu d'achat de tour
+     * @return
+     */
+    private JPanel makeBrownPanel()
+    {
+        JPanel res = new JPanel();
+        // Brown background
+        res.setBackground(new Color(102, 61, 20));
+
+        return res;
+    }
+
+    private JPanel makeMenuPanel()
+    {
+        JPanel res = new JPanel();
+        // Brown background
+        res.setBackground(new Color(102, 61, 20));
+
+        return res;
+    }
+
+    private JPanel makeButtonPanel()
+    {
+        JPanel res = new JPanel();
+        res.setLayout(new GridLayout(1, 3));
+
+        // Button
+
+
+        return res;
+    }
+
+
+    /**
+     * Faire le panel des buttons qui contient acheter, pause et quit
+     * @return
+     */
+    private JPanel makeButton_panel()
+    {
+        JPanel res = new JPanel();
+        res.setBackground(new Color(66, 40, 14));
+
+        /*
+         * 1 - message
+         * 2 - choix pour le tour et menu
+         * 3 - les buttons
+         */
+        res.setLayout(new GridLayout(3, 1));
+
+        res.add(makeMessagePanel());
+        res.add(makeBrownPanel());
+        res.add(makeButtonPanel());
+
+        return res;
+    }
 
 
     private void makeCaros(MapConfig map_config)
