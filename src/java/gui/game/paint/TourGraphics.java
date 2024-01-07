1bca832396b69778051005a4ebfedfbe11244158
EmirhanFaruk <emirhansagir74@gmail.com>
2024-01-07 16:55:06 +0100

    Fix de range + fix d'ortographe pour canon

--- src/java/gui/game/paint/TourGraphics.java
+++ src/java/gui/game/paint/TourGraphics.java
@@ -241,6 +241,6 @@ public class TourGraphics
         int y = (int) tour.getCoordinates().i() * height;
         g.drawImage(image, x, y, width, height, null);
 
-        g.draw(new Rectangle(x - width/2, y - height/2, tour.getRange() * width * 2, tour.getRange() * height * 2));
+        g.draw(new Rectangle((int) (x + width*0.5 - tour.getRange()*width), (int) (y + height*0.5 - tour.getRange()*height), tour.getRange() * width * 2, tour.getRange() * height * 2));
     }
 }
