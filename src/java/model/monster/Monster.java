56c9c0d9b84ae3dffe8a00f00b1503abc1686dc0
EmirhanFaruk <emirhansagir74@gmail.com>
2024-01-07 14:10:00 +0100

    Rajout de mulp

--- src/java/model/monster/Monster.java
+++ src/java/model/monster/Monster.java
@@ -25,14 +25,15 @@ public class Monster {
     private final MapConfig mapConfig ;
     private final static double[][][] mulp =
             {
-                    {{1, 1}, {1.5, 1.1}, {1.8, 1.3}},
-                    {{1, 0.9}, {1.6, 0.9}, {2, 0.85}},
-                    {{1, 1.6}, {1.5, 1.45}, {1.8, 1.45}}
+                    {{1, 1}, {1.5, 1.1}, {1.8, 1.3}, {2, 1.5}},
+                    {{1, 0.9}, {1.5, 0.9}, {1.8, 0.85}, {2, 0.8}},
+                    {{1, 1.6}, {1.5, 1.45}, {1.8, 1.3}, {2, 1.1}}
             }; // live, speed. Exemples a changer
 
     private final static int[][] resistance_pourcentage_avoir =
             {
                     {100, 0, 0, 0},
+                    {70, 10, 10, 10},
                     {40, 20, 20, 20},
                     {10, 30, 30, 30}
             }; // NONE, BULLET, ARROW, FIRE. Pourcentage pour avoir une resistance.
