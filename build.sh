ee68f7882c658def056ad814d2d01c56c471fae5
Mina <xo140597@gmail.com>
2024-01-11 20:00:06 +0100

    Dernier retouches

--- build.sh
+++
@@ -1,9 +0,0 @@
-# Create the output folder if it doesn't exist
-mkdir -p out
-
-# Compile all source files in src/java/com and its subfolders
-find src/java/com/ -name "*.java" -print | xargs javac -d out
-
-echo "Compilation finished."
-
-
--- run.sh
+++
@@ -1,1 +0,0 @@
-java -cp out com.gui.App
